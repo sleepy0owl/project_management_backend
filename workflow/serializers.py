b7d06392a2a893fe49d51be8f78678686645183d
sleepy0owl <souravcristiano502@gmail.com>
2021-06-09 16:52:58 +0530

    added serializers

--- workflow/serializers.py
+++ workflow/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import BoardList, WorkflowBoard
+from .models import BoardList, WorkflowBoard, Card
 class BoardSerializer(serializers.ModelSerializer):
     class Meta:
         model = WorkflowBoard
@@ -12,6 +12,23 @@ class BoardListSerializer(serializers.ModelSerializer):
 
 class CardSerializer(serializers.ModelSerializer):
     class Meta:
-        model = BoardList
+        model = Card
         fields = '__all__'
 
+class CardDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Card
+        fields = ['card_id', "card_name", "card_desciption", "updated_on"]
+
+
+class ListDetailSerializer(serializers.ModelSerializer):
+    card = CardDetailSerializer(many=True)
+    class Meta:
+        model = BoardList
+        fields = ['list_id', 'list_name', 'updated_on', 'card']
+
+class BoardDetailSerializer(serializers.ModelSerializer):
+    board_list = ListDetailSerializer(many=True)
+    class Meta:
+        model = WorkflowBoard
+        fields = ['board_id', 'board_name', 'board_description', 'board_list']
