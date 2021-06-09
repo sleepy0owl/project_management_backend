618528ddb446e87a8c4090e34118fdac74ae56ca
sleepy0owl <souravcristiano502@gmail.com>
2021-06-09 14:30:18 +0530

    added new apis

--- project_management_backend/api.py
+++ project_management_backend/api.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from user_management.views import test_get, create_user
+from user_management.views import test_get, create_user, login_user
+from workflow.views import create_board, get_all_boards, create_list, update_list
 urls = [
     path('test/get/', test_get),
-    path('user/create/', create_user)
+    path('user/create/', create_user),
+    path('user/login/', login_user),
+    path('board/create/', create_board),
+    path('board/getbycustomer/', get_all_boards),
+    path('list/create/', create_list),
+    path('list/update/', update_list)
 ]
